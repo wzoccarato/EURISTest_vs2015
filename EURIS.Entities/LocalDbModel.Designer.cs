f1446946cb53d206b3cfa4c20905e71f5bd4c4bf
walter <walter@BETELGEUZE>
2017-01-01 19:39:35 +0100

    rigenerate le classi di supporto a EF. In web.config c'è la nuova configurazione per modificare il path dove si trova EurisTestDB

--- EURIS.Entities/LocalDbModel.Designer.cs
+++ EURIS.Entities/LocalDbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Progetti\Walter\EURISTest_vs2015\EURIS.Entities\LocalDbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Progetti\Walter\EURISTest_vs2015\EURISTest_vs2015\EURIS.Entities\LocalDbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
