e0b4f254a79070f8af77b1ab4309602ba5d72ac3
Walter <Walter@NETTUNO>
2017-01-09 12:37:24 +0100

    Aggiunta visualizzazione descrizione listino nella pagina di gestione dei listini

--- EURISTest/Properties/AssemblyInfo.cs
+++ EURISTest/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.2.17009")]
-[assembly: AssemblyFileVersion("1.0.2.17009")]
+[assembly: AssemblyVersion("1.0.6.17009")]
+[assembly: AssemblyFileVersion("1.0.6.17009")]
