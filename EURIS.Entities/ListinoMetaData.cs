02f3e67be5377bb5571a6bcc94d1466b0f071e69
walter <walter@BETELGEUZE>
2017-01-10 22:38:59 +0100

    Corretto problema relativo alla validazione nell'inserimento di prodotti e listini. Aggiunte le due buddy classes ProdottoMetaData e ListinoMetaData nel progetto Entities

---
+++ EURIS.Entities/ListinoMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EURIS.Entities
+{
+    [MetadataType(typeof(ListinoMetaData))]
+    public partial class Listino
+    {
+    }
+
+    internal sealed class ListinoMetaData
+    {
+        [Required(ErrorMessage = "Il Codice listino è richiesto")]
+        public string codice;
+    }
+}
---
+++ EURIS.Entities/ProdottoMetaData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EURIS.Entities
+{
+
+    [MetadataType(typeof(ProdottoMetadata))]
+    public partial class Prodotto
+    {
+        // No field here
+    }
+
+    internal sealed class ProdottoMetadata
+    {
+        [Required(ErrorMessage = "Il Codice prodotto è richiesto")]
+        public string codice;
+    
+    }
+}
